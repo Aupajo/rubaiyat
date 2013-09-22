bda597c17da655680a90ae0296d8c3e3498bbdbf
Pete Nicholls <pete@metanation.com>
2013-09-22 15:49:49 +1200

    Site updated to 9731761

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'middleman', '~>3.1.5'
-gem 'middleman-gh-pages'
-gem 'redcarpet'
--- config.rb
+++
@@ -1,9 +0,0 @@
-set :css_dir, 'stylesheets'
-
-set :markdown_engine, :redcarpet
-set :markdown, hard_wrap: true, smartypants: true
-
-configure :build do
-  activate :minify_css
-  activate :asset_hash
-end
